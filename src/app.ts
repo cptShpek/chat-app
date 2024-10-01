cc5ef96c8ccd3f236351ae32e5dd31ba802b1724
cptShpek <tarzansh1397@gmail.com>
2024-10-01 13:39:22 +0200

    remove env

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import helmet from "helmet";
 import cors from "cors";
 import { notFound, errorHandler } from "./middleware/index.middleware";
 import api from "./api/index.api";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 config();
 
@@ -14,6 +16,8 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+app.use(cookieParser());
 
 app.use("/api/", api);
 
