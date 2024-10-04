73ea53741cad307a436593fa01d66d3a7888e2a9
cptShpek <tarzansh1397@gmail.com>
2024-10-04 12:49:04 +0200

    base chats layout

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,5 +1,34 @@
+import { Grid2, Paper } from "@mui/material";
 import React from "react";
+import { Chats } from "./chats";
+import styled from "@emotion/styled";
+
+const Container = styled(Grid2)({
+  height: "calc(100vh - 70px)",
+  maxHeight: "calc(100vh - 70px)",
+});
+
+const Item = styled(Paper)({
+  backgroundColor: "#fff",
+  padding: "5px",
+  textAlign: "center",
+  height: "100%",
+  minHeight: "100%",
+});
 
 export const HomePage: React.FC = () => {
-  return <h1>home</h1>;
+  return (
+    <Container container spacing={1} padding={0.5}>
+      <Grid2 size={2}>
+        <Item>
+          <Chats />
+        </Item>
+      </Grid2>
+      <Grid2 size={10}>
+        <Item>
+          <h1>hello</h1>
+        </Item>
+      </Grid2>
+    </Container>
+  );
 };
